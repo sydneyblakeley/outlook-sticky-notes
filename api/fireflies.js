e764e8ca89d1ef6ec671d12d43db58e494907619
sydneyblakeley <sydneyblakeley@outlook.com>
2026-07-28 11:57:21 -0700

    Update fireflies.js

--- api/fireflies.js
+++ api/fireflies.js
@@ -155,6 +155,8 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   let user;
